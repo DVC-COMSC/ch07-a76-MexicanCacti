09e7057e06a4f72e974ff1a7c3dda0caaf804662
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 21:41:56 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ num2 = list(map(int, input().split()))
 # Make your Code
 # ******************************
 
-if (num1 <= num2):
+if (len(num1) <= len(num2)):
     num3 = num1 + num2
 else:
     num3 = num2 + num1
